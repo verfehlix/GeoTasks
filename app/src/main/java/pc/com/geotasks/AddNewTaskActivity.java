3a237c19aec58e92fdf00b8cf8290a40166fba6f
Felix <felix.p.magnus@gmail.com>
2016-04-12 20:28:46 +0200

    added some functionality to the exit button

--- app/src/main/java/pc/com/geotasks/AddNewTaskActivity.java
+++ app/src/main/java/pc/com/geotasks/AddNewTaskActivity.java
@@ -1,24 +1,82 @@
 package pc.com.geotasks;
 
-import android.graphics.Color;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.widget.ImageButton;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class AddNewTaskActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+    Button exitButton;
+    Button saveButton;
+    EditText editTextTaskName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new_task);
 
-        //init toolbar at the top
-        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
-        setSupportActionBar(myToolbar);
-
+        //setup toolbar
+        toolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
         //hide the title
         getSupportActionBar().setDisplayShowTitleEnabled(false);
 
+        //get buttons
+        exitButton = (Button) findViewById(R.id.exitButton);
+        saveButton = (Button) findViewById(R.id.saveButton);
+
+        //get inputs
+        editTextTaskName = (EditText) findViewById(R.id.editTextTaskName);
+
+    }
+
+    public void exitButtonPressed(View view){
+        //check if any data was entered
+        if(anyDataEntered()){
+            //prompt user if he really wants to exit
+            exitPrompt();
+        } else {
+            //no data was entered, just exit this activity
+            finish();
+        }
+    }
+
+    private void exitPrompt() {
+        AlertDialog exitDialog = new AlertDialog.Builder(AddNewTaskActivity.this).create();
+        exitDialog.setMessage("Do you really want to exit and discard this task?");
+        exitDialog.setButton(AlertDialog.BUTTON_NEGATIVE, "Exit",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                        dialog.dismiss();
+                    }
+                });
+        exitDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "Stay & Keep Editing",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+        exitDialog.show();
+    }
+
+    private boolean anyDataEntered() {
+        boolean anyDataEnterd = false;
+
+        if(textEditFilled(editTextTaskName)) {
+            anyDataEnterd = true;
+        }
+
+        return anyDataEnterd;
+    }
+
+    private boolean textEditFilled(EditText editText){
+        return editText.getText().toString().trim().length() > 0 ? true : false;
     }
 }
