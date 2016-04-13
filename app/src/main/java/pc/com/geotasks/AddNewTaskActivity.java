10958d039dd07271767d506734de294215d9b46a
Felix <felix.p.magnus@gmail.com>
2016-04-13 21:39:32 +0200

    assisted input works

--- app/src/main/java/pc/com/geotasks/AddNewTaskActivity.java
+++ app/src/main/java/pc/com/geotasks/AddNewTaskActivity.java
@@ -1,19 +1,28 @@
 package pc.com.geotasks;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.places.Place;
 import com.google.android.gms.location.places.Places;
+import com.google.android.gms.location.places.ui.PlaceAutocomplete;
 
 public class AddNewTaskActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener, GoogleApiClient.ConnectionCallbacks {
 
@@ -24,6 +33,7 @@ public class AddNewTaskActivity extends AppCompatActivity implements GoogleApiCl
     EditText editTextTaskDescription;
 
     GoogleApiClient googleApiClient;
+    private static final int REQUEST_CODE_AUTOCOMPLETE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,13 +56,13 @@ public class AddNewTaskActivity extends AppCompatActivity implements GoogleApiCl
 
         //init google maps api component
         googleApiClient = new GoogleApiClient
-            .Builder( this )
-            .enableAutoManage( this, 0, this )
-            .addApi( Places.GEO_DATA_API )
-            .addApi( Places.PLACE_DETECTION_API )
-            .addConnectionCallbacks( this )
-            .addOnConnectionFailedListener( this )
+            .Builder(this)
+            .addApi(Places.GEO_DATA_API)
+            .addApi(Places.PLACE_DETECTION_API)
+            .addConnectionCallbacks(this)
+            .addOnConnectionFailedListener(this)
             .build();
+
     }
 
     @Override
@@ -123,9 +133,46 @@ public class AddNewTaskActivity extends AppCompatActivity implements GoogleApiCl
         return editText.getText().toString().trim().length() > 0 ? true : false;
     }
 
+
+
+    public void findPlace(View view) {
+        try {
+            Intent intent =
+                    new PlaceAutocomplete.IntentBuilder(PlaceAutocomplete.MODE_FULLSCREEN).build(this);
+            startActivityForResult(intent, REQUEST_CODE_AUTOCOMPLETE);
+        } catch (GooglePlayServicesRepairableException e) {
+            GoogleApiAvailability.getInstance().getErrorDialog(this, e.getConnectionStatusCode(),0 /* requestCode */).show();
+        } catch (GooglePlayServicesNotAvailableException e) {
+            String message = "Google Play Services is not available: " + GoogleApiAvailability.getInstance().getErrorString(e.errorCode);
+
+            Log.e("findPlace", message);
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    // A place has been received; use requestCode to track the request.
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_CODE_AUTOCOMPLETE) {
+            if (resultCode == RESULT_OK) {
+                Place place = PlaceAutocomplete.getPlace(this, data);
+                Log.i("findPlace", "Place: " + place.getName());
+            } else if (resultCode == PlaceAutocomplete.RESULT_ERROR) {
+                Status status = PlaceAutocomplete.getStatus(this, data);
+                // TODO: Handle the error.
+                Log.i("findPlace", status.getStatusMessage());
+
+            } else if (resultCode == RESULT_CANCELED) {
+                // The user canceled the operation.
+            }
+        }
+    }
+
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-
+        Log.i("api",connectionResult.getErrorMessage());
     }
 
     @Override
